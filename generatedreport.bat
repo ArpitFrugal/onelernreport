656aaba04824feabd2752880771098686590ab14
ShashankFrugal <shashank@frugaltesting.com>
2022-07-07 18:45:31 +0530

    07JUL2022

--- generatedreport.bat
+++ generatedreport.bat
@@ -1,5 +1,5 @@
 set /p id=Enter date: 
-allure generate ../FrugalTesting_Onelern/allure-results -o %id%
+allure generate ../Frugal_Onelern/allure-results -o %id%
 git add %id%
 git commit -m "%id%"
 git push
